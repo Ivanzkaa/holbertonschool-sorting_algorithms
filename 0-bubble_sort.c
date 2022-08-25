c560654a0046f44bc585cbe513fc748cefe8732a
Ivanzkaa <ivanzka@me.com>
2022-08-25 13:41:31 -0400

    using the bubble sorting method for arrays in an ascending order

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ *   bubble_sort - sorting an array of integers in ascending order
+ *   @array: array that points to the integers
+ *   @size: the size of the array
+ *   Return: void
+ **/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, b = 0;
+	int temp = 0;
+
+	for (i = 0; i < size; i++)
+
+	for (b = 0; b < size; b++)
+
+	if (array[b] > array[b + 1])
+	{
+		temp = array[b];
+		array[b] = array[b + 1];
+		array[b + 1] = temp;
+		print_array(array, size);
+	}
+}
