f77c03f4c35ad2037e9d1fc681a18f0758b64dfe
Ivanzkaa <ivanzka@me.com>
2022-08-25 15:05:28 -0400

    sorting a doubly linked list in ascending order

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ *  insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort
+ * @list: double pointer to a doubly sinlgy linked list
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node = NULL, *el_que_lo_aguanta = NULL, *tmp = NULL;
+    *list = NULL;
+
+	for (node = *list; node;)
+	{
+		if (node->next && (node->n > node->next->n))
+		{
+			el_que_lo_aguanta = node->next;
+			for (tmp = el_que_lo_aguanta; tmp->prev; tmp = tmp->prev)
+				if (tmp->prev->n < tmp->n)
+					break;
+
+			if (el_que_lo_aguanta->next && el_que_lo_aguanta->prev)
+			{
+				el_que_lo_aguanta->prev->next = el_que_lo_aguanta->next;
+				el_que_lo_aguanta->next->prev = el_que_lo_aguanta->prev;
+			}
+			else
+			{
+				el_que_lo_aguanta->prev->next = NULL;
+			}
+			el_que_lo_aguanta->next = tmp;
+			if (tmp->prev)
+			{
+				tmp->prev->next = el_que_lo_aguanta->prev = tmp->prev;
+					tmp->prev = el_que_lo_aguanta;
+			}
+			else
+			{
+				tmp->prev = el_que_lo_aguanta->prev = NULL;
+				*list = el_que_lo_aguanta;
+			}
+			print_list(*list), node = *list;
+			continue;
+		}
+		node = node->next;
+	}
+}
