f97cc95e0b635d59f9c57ba4560fff7860ffc70a
Ivanzkaa <ivanzka@me.com>
2022-08-30 17:19:45 -0400

    fixing task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,15 +11,18 @@ void bubble_sort(int *array, size_t size)
 	size_t i = 0, b = 0;
 	int temp = 0;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
 
-	for (b = 0; b < size; b++)
+		for (b = 0; b < size; b++)
 
-	if (array[b] > array[b + 1])
-	{
-		temp = array[b];
-		array[b] = array[b + 1];
-		array[b + 1] = temp;
-		print_array(array, size);
-	}
+			if (array[b] > array[b + 1])
+			{
+				temp = array[b];
+				array[b] = array[b + 1];
+				array[b + 1] = temp;
+				print_array(array, size);
+			}
 }
